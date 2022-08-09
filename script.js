0837c35011a8a13e641c36e615c89af236c8cbeb
Kauan-Tavares <87500818+KauanNogueira@users.noreply.github.com>
2022-08-09 20:42:00 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.querySelector("#btn").addEventListener("click", () => {
-    document.body.classList.toggle("light-mode")
-    const cabecalho = document.querySelector('.cabecalho')
-
-
-})
-
-
