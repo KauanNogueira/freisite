00bf39fcf8ffa07f0b834dc939717d9c45e3ea5a
Kauan-Tavares <87500818+KauanNogueira@users.noreply.github.com>
2022-08-08 21:20:06 -0300

    atualização no site

--- script.js
+++ script.js
@@ -2,5 +2,7 @@ document.querySelector("#btn").addEventListener("click", () => {
     document.body.classList.toggle("light-mode")
     const cabecalho = document.querySelector('.cabecalho')
 
-    cabecalho.style.animation.toggle('cabecalho-claro 2s step-end alternate')
+
 })
+
+
